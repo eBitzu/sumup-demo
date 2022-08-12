23e356eee9f039d367f03d349d93177d644b5faa
Gabriel Raducu <gabriel_raducu@epam.com>
2022-08-12 17:04:08 +0300

    chore: update packages to latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./App.css";
 import { useCardType } from "./services/hooks/use-card-type/use-cart-type";
 import { findCardRange } from "./services/utils/card-find.util";
@@ -28,19 +28,25 @@ function App() {
 
   return ccTypes.length ? (
     <div className="App">
-      <label htmlFor="ccn">Credit Card Number:</label>
-      <br />
-      <input
-        onChange={handleInputChange}
-        type="tel"
-        inputMode="numeric"
-        pattern="[0-9\s]{12,19}"
-        minLength={12}
-        autoComplete="cc-number"
-        maxLength={19}
-        placeholder="xxxx xxxx xxxx xxxx"
-      />
-      <span style={{ marginLeft: "5px" }}>{cardScheme}</span>
+      <small className="disclaimer">
+        This PoC determines the type of credit card based on first initial
+        numbers
+      </small>
+      <div>
+        <label htmlFor="ccn">Credit Card Number: </label>
+        <input
+          onChange={handleInputChange}
+          type="tel"
+          inputMode="numeric"
+          pattern="[0-9\s]{12,19}"
+          minLength={12}
+          autoComplete="cc-number"
+          maxLength={19}
+          placeholder="xxxx xxxx xxxx xxxx"
+        />
+        <span style={{ marginLeft: "5px" }}>{cardScheme}</span>
+      </div>
+      <small className="disclaimer">*This app doesn't store or use this information in any way</small>
     </div>
   ) : (
     <>Loading data</>
